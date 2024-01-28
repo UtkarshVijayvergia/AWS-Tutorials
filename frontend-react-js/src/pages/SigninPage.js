a7473801132d3dcd156cdb89913a63782d2ec409
Utkarsh <utkarsh.v1901@gmail.com>
2024-01-28 17:18:45 +0530

    implement authentication with aws amplify and aws cognito

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -3,27 +3,39 @@ import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
-// [TODO] Authenication
-import Cookies from 'js-cookie'
+// AWS Amplify authenication
+import { Auth } from 'aws-amplify';
 
 export default function SigninPage() {
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  
+  // sign in with aws-amplify and aws cognito
   const [errors, setErrors] = React.useState('');
 
+  // aws-amplify aws cognito - sign in
   const onsubmit = async (event) => {
-    event.preventDefault();
     setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
+    event.preventDefault();
+    Auth.signIn(email, password)
+    .then(user => {
+      localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
       window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
-    }
+    })
+    .catch(error => {
+      if (error.code == 'UserNotConfirmedException') {
+        window.location.href = "/confirm"
+      }
+      setErrors(error.message)
+    });
     return false
   }
+  
+  // let errors;
+  // if (errors){
+  //   errors = <div className='errors'>{errors}</div>;
+  // }
 
   const email_onchange = (event) => {
     setEmail(event.target.value);
